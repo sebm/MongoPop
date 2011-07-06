dc36bafa10dde7f2750d57544f810bec29827f6b
Sebastian Motraghi <sebastian.m@gmail.com>
2011-07-06 00:52:14 -0500

    general port listener thingy

--- app.js
+++ app.js
@@ -129,5 +129,7 @@ app.get('/pop', function(req, res){
   });
 });
 
-app.listen(3000);
+var port = process.env.PORT || 5000;
+
+app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
