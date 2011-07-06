4d92fd1776b2e4e629786d693650505d67f7f6ef
Sebastian Motraghi <sebastian.m@gmail.com>
2011-07-06 11:52:44 -0500

    listen on port 3000

--- app.js
+++ app.js
@@ -132,7 +132,7 @@ app.get('/pop', function(req, res){
   });
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 3000;
 
 app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
